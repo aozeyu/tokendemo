8e54364443fd1b093424d10e59a4a3ec707d1486
yaozeyu112 <3484766443@qq.com>
2022-10-26 21:35:32 +0800

    Initial commit

---
+++ src/main/java/com/example/tokendemo/TokendemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tokendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokendemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokendemoApplication.class, args);
+    }
+
+}
