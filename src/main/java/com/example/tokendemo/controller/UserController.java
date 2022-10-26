409ddee7cb97da5db3071d7ac41877d23e89ae05
yaozeyu112 <3484766443@qq.com>
2022-10-26 22:16:17 +0800

    init

--- src/main/java/com/example/tokendemo/controller/UserController.java
+++ src/main/java/com/example/tokendemo/controller/UserController.java
@@ -1,2 +1,47 @@
-package com.example.tokendemo.controller;public class UserController {
+package com.example.tokendemo.controller;
+
+
+import cn.dev33.satoken.annotation.SaCheckLogin;
+import cn.dev33.satoken.stp.StpUtil;
+import cn.dev33.satoken.util.SaResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/")
+public class UserController {
+    @RequestMapping("doLogin")
+    public SaResult doLogin(String username, String password) {
+        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
+        if ("zhang".equals(username) && "123456".equals(password)) {
+            StpUtil.login(10001);
+            //使用封装的请求结果
+            return SaResult.ok("登录成功");
+        }
+        return  SaResult.error("登录失败");
+    }
+// 查询登录状态，浏览器访问： http://localhost:8081/user/isLogin
+    @RequestMapping("isLogin")
+    public SaResult isLogin() {
+        return SaResult.ok("是否登录：" + StpUtil.isLogin());
+    }
+    // 查询 Token 信息
+    @RequestMapping("tokenInfo")
+    public SaResult tokenInfo() {
+        return SaResult.data(StpUtil.getTokenInfo());
+    }
+
+
+    // 测试注销  ---- http://localhost:8081/user/logout
+    @RequestMapping("logout")
+    public SaResult logout() {
+        StpUtil.logout();
+        return SaResult.ok();
+    }
+
+    @SaCheckLogin
+    @RequestMapping("info")
+    public SaResult info() {
+        return SaResult.ok("查询信息");
+    }
 }
